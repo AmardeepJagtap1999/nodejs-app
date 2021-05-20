3bb354ae1990fd49080e59df5fa0a126e6f8dc22
AmardeepJagtap1999 <61075436+AmardeepJagtap1999@users.noreply.github.com>
2021-05-20 18:53:04 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.232.123.23:9000/',
+  serverUrl: 'http://10.100.20.44:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
